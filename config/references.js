28e59b475092caf0a567aa6e259b438e4507a739
Brett Hevia <brett.hevia@gmail.com>
2017-07-09 19:17:48 -0400

    removing ignored files

--- config/references.js
+++
@@ -1,5 +0,0 @@
-import heviaFirebase from './firebase';
-
-export const getUsersRef = (name) => heviaFirebase.database().ref(`users/${name}`)
-
-export const getMessagesRef = () => heviaFirebase.database().ref(`messages`)
